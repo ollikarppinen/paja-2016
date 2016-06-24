02f7a27837cfdfdac2c4d6474bfa52d9da904ece
Olli Karppinen <ollikarppinen@gmail.com>
2016-06-24 22:29:56 +0300

    Adds colors to d3 line chart

--- d3-line-chart/src/index.js
+++ d3-line-chart/src/index.js
@@ -1,7 +1,7 @@
-// import * as chart from './chart';
+ // import * as chart from './chart';
 import d3 from 'd3';
 
-const margin = {top: 30, right: 20, bottom: 30, left: 50};
+const margin = {top: 30, right: 20, bottom: 70, left: 50};
 const width = 600 - margin.left - margin.right;
 const height = 270 - margin.top - margin.bottom;
 
@@ -36,47 +36,69 @@ const durationLine = d3.svg.line()
   .y(d => y(d.duration));
 
 d3.json('data.json', (error, dataSets) => {
-  const dataSetsParsed = dataSets.map(ds =>
-    ds.data.map(d =>
+  const color = d3.scale.category10();
+  const dataSetsParsed = dataSets.map((ds, i) =>
       ({
-        startTime: parseDate(d.startTime),
-        duration: +d.duration,
-        id: d.id
+        id: i,
+        color: color(i),
+        label: ds.label,
+        data: ds.data.map(d =>
+          ({
+            startTime: parseDate(d.startTime),
+            duration: +d.duration,
+            id: d.id
+          })).sort(sortByDateAscending)
       })
-    ).sort(sortByDateAscending)
   );
 
-  const dataArray = dataSetsParsed.reduce((a, b) => a.concat(b));
+  const dataArray = dataSetsParsed
+    .map(d => d.data)
+    .reduce((a, b) => a.concat(b));
 
   x.domain(d3.extent(dataArray, d => d.startTime));
   y.domain([0, d3.max(dataArray, d => d.duration)]);
 
-  dataSetsParsed.forEach(ds => {
+  const legendSpace = width / dataSetsParsed.length;
+
+  dataSetsParsed.forEach((ds, i) => {
+    const data = ds.data;
+    // console.log(ds);
     svg.append('path')
-    .attr('class', 'line')
-    .attr('d', durationLine(ds));
+      .attr('class', 'line')
+      .style('stroke', ds.color)
+      .attr('d', durationLine(data));
+
+    svg.append('text')
+      .attr('x', (legendSpace / 2) + i * legendSpace)
+      .attr('y', height + (margin.bottom / 2) + 5)
+      .attr('class', 'legend')
+      .style('fill', ds.color)
+      .text(ds.label);
 
     svg.selectAll('dot')
-    .data(ds)
-    .enter()
-    .append('circle')
-    .attr('r', 4)
-    .attr('cx', d => x(d.startTime))
-    .attr('cy', d => y(d.duration))
-    .on('mouseover', d => {
-      div.transition()
-      .duration(200)
-      .style('opacity', .9);
-      div	.html(formatTime(d.startTime) + '<br/>'  + d.duration)
-      .style('left', (d3.event.pageX) + 'px')
-      .style('top', (d3.event.pageY - 28) + 'px');
-    })
-    .on('mouseout', () => {
-      div.transition()
-      .duration(500)
-      .style('opacity', 0);
-    })
-    .on('click', d => console.log(d));
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('fill', ds.color)
+      .attr('r', 4)
+      .attr('cx', d => x(d.startTime))
+      .attr('cy', d => y(d.duration))
+      .on('mouseover', d => {
+        div.transition()
+          .duration(200)
+          .style('opacity', .9);
+        div.html(formatTime(d.startTime) + '<br/>'  + d.duration)
+          .style('left', (d3.event.pageX) + 'px')
+          .style('top', (d3.event.pageY - 28) + 'px')
+          .style('background-color', ds.color)
+          .style('color', 'white');
+      })
+      .on('mouseout', () => {
+        div.transition()
+        .duration(500)
+        .style('opacity', 0);
+      })
+      .on('click', d => console.log(d));
   });
 
   svg.append('g')
